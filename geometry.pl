eaaf3cca11a37e69336b4f28e552c0fb45ffd5df
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-06-27 16:54:15 -0500

    progress

--- geometry.pl
+++ geometry.pl
@@ -9,5 +9,13 @@ area(square, Side, X) :- {X = Side^2}.
 % area(rectangle, Width, Height, X) :- X is Width * Height.
 area(rectangle, Width, Height, X) :- {X = Width * Height}.
 
-double(N, R) :- R is N * 2.
+% This is an alternate implementation from @sifb
+% in the Prolog Discord channel on 6/27/23.
+radius_area(R, A) :-
+    ground(R), % tests whether R is not a free variable
+    A is pi * R^2.
+
+radius_area(R, A) :-
+    ground(A), % tests whether A is not a free variable
+    R is sqrt(A / pi).
 
--- geometry.plt
+++ geometry.plt
@@ -5,14 +5,17 @@
 
 near(A, B) :- abs(A - B) =< 1e-7.
 
-test(double) :-
-  double(3, X), X =:= 6.
-
 test(area) :-
   area(circle, 3, X), near(X, 28.27433388231), !.
 
 test(area) :-
   area(circle, X, 10), near(X, 1.784124116153), !.
+
+test(compute_circle_area) :-
+  radius_area(3, X), near(X, 28.27433388231), !.
+
+test(compute_circle_radius) :-
+  radius_area(X, 10), near(X, 1.784124116153), !.
 
 :- end_tests(geometry).
 :- run_tests.
