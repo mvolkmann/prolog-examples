5871cbd0ad9cd37f7c3b48fd9118f45cd3ab6ecc
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-07-03 14:36:48 -0500

    progress

--- pairs.pl
+++ pairs.pl
@@ -1,5 +1,5 @@
 % These rules describe board positions with row-column pairs.
-queen_can_attack(R-_, R-_).
-queen_can_attack(_-C, _-C).
+queen_can_attack(R-_, R-_) :- !.
+queen_can_attack(_-C, _-C) :- !.
 queen_can_attack(R1-C1, R2-C2) :-
   abs(R1 - R2) =:= abs(C1 - C2).
