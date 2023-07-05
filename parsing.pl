5822d794be1417c3e211ccbf9d9493db4d0c1bcd
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-07-05 14:44:11 -0500

    progress

--- parsing.pl
+++ parsing.pl
@@ -7,10 +7,11 @@ expr_rest --> "+", expr.
 % phrase(expr, "1+1+1"), false. % adding , false allows it to terminate
 */
 
-% This uses eager consumers which cause
+% This uses the "eager consumer rule" which causes
 % tokens to extend to their maximum length.
 % Eager consumes check for the nil case ([]) last.
 % Curly braces on needed here to evaluate a Prolog predicate in a DCG rule.
+% The char_type predicate here asserts that H is an alphabetic character.
 word([H|T]) --> [H], { char_type(H, alphabetic) }, word(T).
 word([]) --> [].
 
@@ -18,4 +19,8 @@ words([]) --> [].
 words([H|T]) --> ws, word(H), ws, words(T).
 
 ws --> [W], { char_type(W, whitespace) }, ws | [].
-% phrase(words(Ws), "The quick brown fox jumps over the lazy dog.").
+
+% The once predicate wraps another predicate and gives only the first solution.
+% once(phrase(words(Ws), "The quick brown fox jumps over the lazy dog")).
+% This does not terminate if the string contains
+% characters that are not alphabetic or whitespace such as a period.
