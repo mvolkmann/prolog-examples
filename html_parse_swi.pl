7507f2c5cf715bb8be17e5debee80a3dcd94656e
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-07-05 19:50:05 -0500

    progress

--- html_parse_swi.pl
+++ html_parse_swi.pl
@@ -2,12 +2,13 @@
 :- use_module(library(http/http_open)). % for http_open
 
 % N occurrences of the ASCII code A is described by the string S. 
-% For example, repeat(5, 32, S). sets S to a string containing 5 spaces.
+% For example, repeat(5, 32, S) sets S to a string containing 5 spaces.
 repeat(N, A, S) :-
   length(L, N),
   maplist(=(A), L),
   atom_codes(S, L).
 
+% This is used if 2nd argument is an element structure.
 % element structure components are Tag, Attributes, and Children.
 print_tag(Level, element(Tag, _, Children)) :-  %
   Indent is Level * 2,
@@ -16,13 +17,14 @@ print_tag(Level, element(Tag, _, Children)) :-  %
   NextLevel is Level + 1,
   maplist(print_tag(NextLevel), Children).
 
-% This is used if 1st argument is not an element structure.
+% This is used if 2nd argument is not an element structure.
 print_tag(_, _). % ignore
 
+% This provides a starting level of 0.
 print_tag(E) :- print_tag(0, E).
 
 process(In) :-
-  % copy_stream_data(In, user_output).
+  % copy_stream_data(In, user_output). % for debugging
   load_html(In, DOM, []),
   maplist(print_tag, DOM).
 
