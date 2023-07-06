0bdf0d69b94a7f3abc4aa51a2464602db776255e
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-07-06 09:09:43 -0500

    progress

--- html_parse_swi.pl
+++ html_parse_swi.pl
@@ -1,19 +1,15 @@
 % This was only tested in SWI-Prolog.
 :- use_module(library(http/http_open)). % for http_open
 
-% N occurrences of the ASCII code A is described by the string S. 
-% For example, repeat(5, 32, S) sets S to a string containing 5 spaces.
-repeat(N, A, S) :-
-  length(L, N),
-  maplist(=(A), L),
-  atom_codes(S, L).
+indent_write(Indent, V) :-
+  (between(1, Indent, _), write(' '), false);
+  writeln(V).
 
 % This is used if 2nd argument is an element structure.
 % element structure components are Tag, Attributes, and Children.
 print_tag(Level, element(Tag, _, Children)) :-
   Indent is Level * 2,
-  repeat(Indent, 32, Spaces),
-  write(Spaces), write(Tag), nl,
+  indent_write(Indent, Tag),
   NextLevel is Level + 1,
   maplist(print_tag(NextLevel), Children).
 
@@ -28,9 +24,11 @@ process(In) :-
   load_html(In, DOM, []),
   maplist(print_tag, DOM).
 
+shutdown(In) :- close(In), halt.
+
 :- setup_call_cleanup(
      % Must use single, not double quotes around URL!
      http_open('https://mvolkmann.github.io', In, []),
      process(In),
-     close(In)).
+     shutdown(In)).
 
