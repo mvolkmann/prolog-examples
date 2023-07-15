70ba592d38470dc0aa64659d7c80c2788c2cef15
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-07-15 15:26:28 -0500

    progress

--- n-queens.pl
+++ n-queens.pl
@@ -1,13 +1,26 @@
-% See "N-Queens in Prolog" at https://www.youtube.com/watch?v=l_tbL9RjFdo.
-% This code can test, complete, and generate solutions.  For example:
-% The following tests whether a given solution is valid.
-% - n_queens(4, [2, 4, 1, 3]).
-% The following completes the solution beginning with 2 and 4.
-% - n_queens(4, [2, 4, X, Y]).
-% The following generates all possible solutions for a board size of 4.
-% - n_queens(4, L), labeling(L).
+/*
+See "N-Queens in Prolog" at https://www.youtube.com/watch?v=l_tbL9RjFdo.
+This code can test, complete, and generate solutions.  For example:
+
+The following tests whether a given solution is valid:
+  `n_queens(4, [2, 4, 1, 3]).`
+
+The following completes the solution beginning with 2 and 4:
+  `n_queens(4, [2, 4, X, Y]).`
+
+The following generates all possible solutions for a board size of 4:
+  `n_queens(4, L), labeling(L).`
+*/
 
 :- use_module(library(clpfd)).
+/* For Scryer Prolog animation ...
+:- use_module(library(clpz)).
+:- use_module(library(lists)).
+:- use_module(library(format)).
+:- use_module(library(dcgs)).
+:- use_module(library(freeze)).
+:- use_module(library(charsio)).
+*/
 
 n_queens(N, Qs) :-
   length(Qs, N), % number of elements in list Qs must be N
@@ -40,23 +53,51 @@ safe_queens_(Q0, [Q|Qs], Distance) :-
   NextDistance #= Distance + 1,
   safe_queens_(Q0, Qs, NextDistance).
 
+/* For Scryer Prolog animation ...
+postscript -->
+  "/init { /N exch def 340 N div dup scale -1 -1 translate \
+           /Palatino-Roman 0.8 selectfont 0 setlinewidth \
+           1 1 N { 1 1 N { 1 index c } for pop } for } bind def \
+   /r { translate 0 0 1 1 4 copy rectfill 0 setgray rectstroke } bind def \
+   /i { gsave 0.5 setgray r grestore } bind def \
+   /q { gsave r 0.5 0.28 translate (Q) dup stringwidth pop -2 div 0 moveto \
+        1 setgray show grestore } bind def \
+   /c { gsave 1 setgray r grestore } bind def\n".
+
+show(N, Options) :-
+  N #> 0,
+  n_queens(N, Qs),
+  phrase(postscript, Ps),
+  format("~s ~w init\n", [Ps,N]),
+  animate(Qs),
+  labeling(Options, Qs),
+  get_single_char(_),
+  false.
+show(_, _ ) :- halt.
+*/
+
 :- initialization
-  N = 4,
+  N = 20,
 
   /*
-  % "Generate and Test" approach - very slow
+  % "Generate and Test" approach - very slow;
+  % 17 seconds with N = 8; seemingly forever with N = 20 
+  length(Qs, N),
   % Generate every possible combination of queen positions.
   maplist(between(1, N), Qs),
   % Determine which of the combinations are solutions.
   n_queens(N, Qs),
   */
 
-  % "Early Prunning" approach - better
+  /*
+  % "Early Prunning" approach - better; 4.32 seconds with N = 20
+  % The length predicate is not needed with this approach
+  % because it is enforced in the "n_queens" predicate.
   n_queens(N, Qs), % much faster with this before next line
   maplist(between(1, N), Qs),
+  */
 
-  /*
-  % "Intelligent Search" approach - much better
+  % "Intelligent Search" approach - much better; 0.11 seconds with N = 20
   n_queens(N, Qs),
   % The labeling predicate provides constraint propagation.
   % This avoids evaluating inconsistent solutions.
@@ -72,7 +113,7 @@ safe_queens_(Q0, [Q|Qs], Distance) :-
   % There are more predefined search strategies
   % and you can define custom strategies.
   labeling([ff], Qs),
-  */
 
-  writeln(Qs).
-  % halt.
+  writeln(Qs),
+  % phrase(format('Qs = ~w~n', [Qs])).
+  halt.
