6befd90d45276126ebc7e15f264b97f8e914414d
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-07-18 15:15:04 -0500

    progress

--- dynamic.pl
+++ dynamic.pl
@@ -4,28 +4,38 @@ fruit(banana).
 person(mark, 74).
 person(tami, 65).
 
-tall(N) :-
-  person(N, H),
-  H >= 72. 
+% Rules succeed or fail.
+% They do not return a value.
+tall(N) :- person(N, H), H >= 72. 
 
 add_predicates :-
+  % Dynamically add a fruit fact.
   dynamic(fruit/1),
   assertz(fruit(cherry)),
 
+  % Dynamically add a tall fact.
   dynamic(tall/1),
-  assertz(tall(giraffe)).
+  assertz(tall(giraffe)),
+
+  % There is no reason to add this particular rule dynamically,
+  % but this demonstates that it is possible.
+  dynamic(sum/3),
+  assertz(sum(X, Y, Z) :- Z is X + Y).
 
 report(Thing) :-
   tall(Thing) ->
-    format('~w is tall.~n', [Thing]);
-    format('~w is not tall.~n', [Thing]).
+    report_(Thing, 'tall');
+    report_(Thing, 'not tall').
+
+% Auxiliary rule names end in an underscore by convention.
+report_(Thing, X) :- format('~w is ~w.~n', [Thing, X]).
 
 :- initialization
   add_predicates,
-
   findall(F, fruit(F), Fruits),
   format('Fruits = ~w~n', [Fruits]), % [apple, banana, cherry]
-
   Things = [mark, tami, giraffe],
   maplist(report, Things),
+  sum(2, 3, S),
+  format('sum is ~w~n', [S]), % sum is 5
   halt.
