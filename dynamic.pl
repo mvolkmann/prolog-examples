c7962b26364bdd67b5fc98b7733fb6865048b77f
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-07-19 08:21:54 -0500

    progress

--- dynamic.pl
+++ dynamic.pl
@@ -33,9 +33,9 @@ report_(Thing, X) :- format('~w is ~w.~n', [Thing, X]).
 :- initialization
   add_predicates,
   findall(F, fruit(F), Fruits),
-  format('Fruits = ~w~n', [Fruits]), % [apple, banana, cherry]
+  writeln(Fruits), % [apple, banana, cherry]
   Things = [mark, tami, giraffe],
   maplist(report, Things),
   sum(2, 3, S),
-  format('sum is ~w~n', [S]), % sum is 5
+  writeln(S), % 5
   halt.
