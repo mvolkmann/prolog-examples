221226ac6352cc7caced0976455f4efe467ec5a6
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-07-26 10:47:06 -0500

    progress

--- search.pl
+++ search.pl
@@ -10,6 +10,7 @@ edge(c, g).
 edge(g, h).
 edge(h, i).
 
+/*
 from_to_path(From, To, Path) :-
   from_to_path_(From, To, P),
   append([From], P, Path).
@@ -22,11 +23,24 @@ from_to_path_(From, To, [Node|Path]) :-
   format("evaluated ~w~n", [From]),
   edge(From, Node),
   from_to_path_(Node, To, Path).
+*/
+
+from_to_path(From, To, Path) :- from_to_path_(From, To, Path).
+
+from_to_path_(Node, Node, [Node]).
+
+from_to_path_(From, To, [To|Path]) :-
+  format("searching for path from ~w to ~w~n", [From, To]),
+  from_to_path_(From, ToParent, Path),
+  edge(ToParent, To).
+  % \+ member(To, Path). % avoids cycles
 
 :- initialization((
-  length(Path, _),
-  from_to_path(a, i, Path),
-  format("path is ~w~n", Path)
+  /*
+  % length(Path, _),
   % format("length is ~d~n", L)
+  */
+  from_to_path(a, i, Path),
+  format("path is ~w~n", [Path])
 )).
 
