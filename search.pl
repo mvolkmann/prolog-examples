5f728912f2e89aff700709faf4b0ffe086bcf107
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-07-26 10:34:30 -0500

    progress

--- search.pl
+++ search.pl
@@ -1,27 +1,32 @@
 :- use_module(library(format)).
 :- use_module(library(lists)).
 
-room(a, [b, c]).
-room(b, [d, e]).
-room(c, [f, g]).
-room(d, []).
-room(e, []).
-room(f, []).
-room(g, []).
+edge(a, b).
+edge(a, c).
+edge(b, d).
+edge(b, e).
+edge(c, f).
+edge(c, g).
+edge(g, h).
+edge(h, i).
 
-% Find the shortest path from a starting room to a goal room.
-solve(Start, Goal, Path) :- search(Goal, [], Path, Start).
+from_to_path(From, To, Path) :-
+  from_to_path_(From, To, P),
+  append([From], P, Path).
 
-search(Goal, P0, P, Start) :-
-  format("evaluating room ~w~n", [Start]),
-  append(P0, [Goal], P),
-  (Start == Goal ->
-    write(solved), nl;
-    room(Start, Adjacent),
-    maplist(search(Goal, P, P1), Adjacent)
-  ).
+from_to_path_(From, To, [To]) :-
+  edge(From, To),
+  format("evaluated ~w~n", [From]).
+
+from_to_path_(From, To, [Node|Path]) :-
+  format("evaluated ~w~n", [From]),
+  edge(From, Node),
+  from_to_path_(Node, To, Path).
 
 :- initialization((
-  solve(a, g, Path),
-  write(Path), nl
+  length(Path, _),
+  from_to_path(a, i, Path),
+  format("path is ~w~n", Path)
+  % format("length is ~d~n", L)
 )).
+
