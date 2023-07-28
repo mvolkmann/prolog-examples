a0aa51e71effbde592688938c7638e0852f93eaa
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-07-28 16:27:15 -0500

    progress

--- family.pl
+++ family.pl
@@ -1,17 +1,43 @@
 female(amanda).
+female(gerri).
+female(judi).
 female(tami).
+
 male(clarence).
 male(jeremy).
 male(mark).
 male(richard).
+
 father(clarence, tami).
 father(richard, mark).
 father(mark, amanda).
 father(mark, jeremy).
+
+mother(judi, mark).
+mother(gerri, tami).
 mother(tami, amanda).
 mother(tami, jeremy).
 
 grandfather(X, Y) :-
   male(X),
   father(X, Z),
-  (father(Z, Y); mother(Z, Y)).
+  (father(Z, Y); mother(Z, Y)),
+  !.
+
+is_father(X) :- father(X, _), !.
+
+is_mother(X) :- mother(X, _), !.
+
+is_son(X) :-
+  (father(_, X); mother(_, X)), !.
+
+sibling(X, Y) :-
+  father(F, X),
+  father(F, Y),
+  mother(M, X),
+  mother(M, Y).
+
+sister(X, Y) :-
+  \+ X = Y, % can't be sister of self
+  female(X),
+  sibling(X, Y).
---
+++ family.plt
@@ -0,0 +1,34 @@
+% To run these tests, enter `swipl family.plt`
+:- consult(family).
+:- begin_tests(family).
+
+test(grandfather) :-
+  grandfather(clarence, jeremy),
+  grandfather(clarence, amanda),
+  grandfather(richard, jeremy),
+  grandfather(richard, amanda).
+
+test(is_father) :-
+  is_father(clarence),
+  is_father(richard),
+  is_father(mark).
+
+test(is_mother) :-
+  is_mother(gerri),
+  is_mother(judi),
+  is_mother(tami).
+
+test(is_son) :-
+  is_son(jeremy),
+  is_son(mark).
+
+test(sibling) :-
+  sibling(amanda, jeremy).
+
+test(sister) :-
+  sister(amanda, jeremy),
+  \+ sister(amanda, amanda).
+
+:- end_tests(family).
+:- run_tests.
+:- halt.
