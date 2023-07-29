ce70ac0733ceb7685eeb885d37039bad08be0cb1
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-07-29 12:14:32 -0500

    progress

--- http_server_scryerp.pl
+++ http_server_scryerp.pl
@@ -4,6 +4,9 @@ text_handler(_, Response) :-
   http_status_code(Response, 200),
   http_body(Response, text("Welcome to Scryer Prolog!")).
 
-http_listen(8081, [
-  get(echo, text_handler)                % GET /echo
-]).
+:- initialization(
+  % GET /echo
+  http_listen(8081, [
+    get(echo, text_handler)
+  ])
+).
