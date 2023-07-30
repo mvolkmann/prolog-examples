db4bbc6c247d8214ea96eec401e1e2397e821754
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-07-30 14:33:49 -0500

    progress

---
+++ throw_catch.pl
@@ -0,0 +1,39 @@
+:- use_module(library(clpz)).
+
+double(N, D) :-
+  ( N #>= 0 ->
+    D #= N * 2
+  ; throw(error(
+      domain_error(non_negative_integer, N),
+      double/2
+    ))
+  ).
+
+handle(Error) :-
+  format("received error ~w~n", Error).
+  % format("context is ~w~n", Context),
+  % fail.
+
+demo :-
+  catch(
+    % The first argument specifies the goal to try.
+    double(-3, D),
+
+    % The second argument specifies the kinds of errors to handle.
+    % Passing a variable as the first argument of `error`
+    % allows it to catch any kind of error.
+    % Pass `error(specific_error_type, _)` to only catch a specfic kind.
+    error(domain_error(Domain, Value), Context),
+    % Error,
+
+    % The third argument specifies what to do after the error is thrown.
+    (
+      format(
+        "~w was passed ~w which is not in the domain ~w.~n",
+        [Context, Value, Domain]
+      ),
+      % fail
+      D = 0
+    )
+  ),
+  format("D = ~d~n", [D]).
