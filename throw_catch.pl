fff740ea1df52c7d48e1ace7ec371ec042382b92
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-07-30 15:23:27 -0500

    progress

--- throw_catch.pl
+++ throw_catch.pl
@@ -10,32 +10,32 @@ double(N, D) :-
     ))
   ).
 
-handle(Error) :-
-  format("received error ~w~n", Error).
-  % format("context is ~w~n", Context),
-  % fail.
-
 demo :-
   catch(
     % The first argument specifies the goal to try.
     double(-3, D),
 
     % The second argument specifies the kinds of errors to handle.
-    % Passing a variable as the first argument of `error`
-    % allows it to catch any kind of error.
-    % Pass `error(specific_error_type, _)` to only catch a specfic kind.
     error(domain_error(Domain, Value), Context),
-    % error(Kind(Domain, Value), Context),
+
+    % The second argument can also be a variable to catch any kind of error.
     % Error,
 
-    % The third argument specifies what to do after the error is thrown.
+    % The third argument specifies what to do after an error is caught.
     (
       format(
         "~w was passed ~w which is not in the domain ~w.~n",
         [Context, Value, Domain]
       ),
+
+      % Use this instead of the previous line
+      % when the second argument is a variable.
       % format("Error = ~w~n", [Error]),
+
+      % optionally fail this rule when an error is caught.
       % fail
+
+      % This provides a value for D when an error is caught.
       D = 0
     )
   ),
