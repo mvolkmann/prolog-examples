346d410c5472b87b3ae0f9a26f2858a18aff8410
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-08-05 09:44:20 -0500

    progress

--- module_issue/main.pl
+++ module_issue/main.pl
@@ -1,13 +1,13 @@
 :- use_module(my_module).
 
-:- meta_predicate(goal0_qualified(0, -)).
-goal0_qualified(G0, G0).
+:- meta_predicate(predicate0_goal(0, -)).
+predicate0_goal(G, G).
 
 first :- write('in first'), nl.
 
 second :- write('in second'), nl.
 
 demo :-
-  goal0_qualified(first, G1),
-  goal0_qualified(second, G2),
+  predicate0_goal(first, G1),
+  predicate0_goal(second, G2),
   call_goals([G1, G2]).
