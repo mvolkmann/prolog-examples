4a9b2b4f847fbdb6e20fcd7d3606faf746088500
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-08-08 15:13:09 -0500

    progress

--- tfilter.pl
+++ tfilter.pl
@@ -1,17 +1,18 @@
-:- use_module(library(reif)).
+:- use_module(library(clpz)). % for zcompare
+:- use_module(library(reif)). % for tfilter
 
 % Greater or equal
 ge(=, true).
 ge(>, true).
 ge(<, false).
 
-has_length(List, Bool) :-
-  length(List, Length),
-  zcompare(Compare, Length, 6),
-  ge(Compare, Bool).
+length_at_least(Length, List, Bool) :-
+  length(List, Len),
+  zcompare(Compare, Len, Length), % sets Compare to =, >, or <
+  ge(Compare, Bool). % sets Bool to true or false
 
 demo :-
   L0 = ["apple", "banana", "cherry", "date"],
-  tfilter(has_length, L0, L1),
-  write(L1), nl. % ["banana", "cherry"]
+  tfilter(length_at_least(6), L0, L),
+  L == ["banana", "cherry"]. % succeeds
 
